42430730202ddeb769a284282ae69516564701a1
Jenitta James <jenittaeliz301@gmail.com>
2025-04-23 19:03:08 +0530

    added new problem

--- Week 1/array.js
+++ Week 1/array.js
@@ -15,3 +15,20 @@ function reversal(num){
 
 
 console.log(reversal(arr));
+
+
+// finding the frequency of elements in an array
+
+let data = [1,2,3,4,5];
+
+function frequencyFinder(data){
+    let obj = {};
+
+    for(let val of data){
+        obj[val] = (obj[val] || 0) + 1;
+    }
+
+    return obj;
+}
+
+console.log(frequencyFinder(data));
