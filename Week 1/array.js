dfe36dc99626851e81d31f1a4bb4d909ec7dad2f
Jenitta James <jenittaeliz301@gmail.com>
2025-05-08 19:28:35 +0530

    binary,linear,linkedlist

--- Week 1/array.js
+++ Week 1/array.js
@@ -32,3 +32,40 @@ function frequencyFinder(data){
 }
 
 console.log(frequencyFinder(data));
+
+
+// find maximum element
+
+let arr1 = [1,2,5,9,3,3,4,5]
+
+function max(arr1){
+    let max= arr1[0]
+
+    for(let i = 0; i<arr1.length; i++){
+        if(max<arr1[i]){
+            max = arr1[i]
+        }
+    }
+    return max
+}
+
+
+console.log(max(arr1))
+
+
+// check if an array contains duplicate
+
+let ar1 = [3,5,7,9,6,7]
+
+function duplicate(ar1){
+    for(let i = 0;i<ar1.length;i++){
+        if(ar1.indexOf(ar1[i])!==ar1.lastIndexOf(ar1[i])){
+            return true
+        }
+        
+    }
+    return false
+}
+
+
+console.log(duplicate(ar1));
